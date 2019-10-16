299581d5ff80386bce8a4ff195f44f990159491e
Andrew Wood <ajwood2001@gmail.com>
2019-10-16 15:34:18 +0100

    First three tests passed

---
+++ models/traveller.js
@@ -0,0 +1,34 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+  return journey.startLocation;
+});
+}
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+  return journey.endLocation;
+});
+}
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
